e295be14751cfcfef0f2be0c0ed8eb5fde688c96
ArdaCan271 <ardacan271@gmail.com>
2024-01-14 02:11:32 +0300

    upload page done

--- screens/UploadPage.js
+++ screens/UploadPage.js
@@ -1,12 +1,21 @@
-//use rnfes but put semicolons at the end of the line
-
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const UploadPage = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcomeText}>Upload Page</Text>
+      <View style={styles.uploadArea}>
+        <View style={{justifyContent: "center", alignItems: "center"}}>
+          <Ionicons name="cloud-upload" size={50} color="#897de3" />
+          <Text style={styles.uploadText}>Upload your files</Text>
+        </View>
+        <Pressable style={styles.uploadButton}>
+          <Ionicons name="arrow-up-circle-outline" size={24} color="white" />
+          <Text style={{fontSize: 18, fontWeight: "500", marginLeft: -12}}>Upload</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -19,11 +28,31 @@ const styles = StyleSheet.create({
     height: "100%",
     width: "100%",
     backgroundColor: "white",
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
   },
-  welcomeText: {
+  uploadArea: {
+    height: "35%",
+    width: "85%",
+    backgroundColor: "#ccc1e3",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    marginTop: "20%",
+    borderRadius: 15,
+  },
+  uploadText: {
     fontSize: 24,
-    color: "black",
-  }
+    fontWeight: "500",
+    marginBottom: 10,
+    color: "#897de3",
+  },
+  uploadButton: {
+    height: 50,
+    width: 110,
+    backgroundColor: "#655DBB",
+    flexDirection: "row",
+    borderRadius: 10,
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
 });
